630a45b012b1fe38c1fb21af6d80c5e4ca6b6853
Dhiraj Sharma <dhirajsharma072@gmail.com>
2019-04-17 00:37:32 +0800

    1.added form to create token 2.refactored app.js

--- app.js
+++ app.js
@@ -1,15 +1,16 @@
 const createError = require('http-errors')
 const express = require('express')
 const path = require('path')
+const http = require('http');
 const cookieParser = require('cookie-parser')
 const logger = require('morgan')
-
 const authRouter = require('./routes/auth')
+const authController = require('./controllers/auth')
 
 const app = express()
-// view engine setup
-app.set('views', path.join(__dirname, 'views'))
-app.set('view engine', 'jade')
+app.server = http.createServer(app)
+const io = require('socket.io')(app.server);
+
 
 app.use(logger('dev'))
 app.use(express.json())
@@ -17,22 +18,32 @@ app.use(express.urlencoded({extended: false}))
 app.use(cookieParser())
 app.use(express.static(path.join(__dirname, 'public')))
 
+io.use(authController.verifyToken)
 app.use('/', authRouter)
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use((req, res, next) => {
     next(createError(404))
 })
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-    res.locals.message = err.message
-    res.locals.error = req.app.get('env') === 'development' ? err : {}
 
-  // render the error page
-    res.status(err.status || 500)
-    res.render('error')
-})
+app.use((err, req, res, next) => {
+    const error = {
+        status: err.status || 500,
+        message: err.message || 'Server Error',
+    };
+    res.status(err.status).json(error);
+});
 
-module.exports = app
+io.on('connection', (socket) => {
+    setInterval(() => {
+        socket.emit('time', {
+            time: Date()
+        });
+    }, 5000)
+});
+
+const listener = app.server.listen(process.env.PORT || 3000, '127.0.0.1', () => {
+    console.log(`Server started on  http://${listener.address().address + ':' + listener.address().port}`);
+});
+
